faef05556ab0eebd1e4adb8f5682bfc6c5971222
mikhailantoshkin <mike.antoshkin@gmail.com>
2025-06-24 13:43:35 +0900

    elixir

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -7,8 +7,7 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
-  -- { import = "astrocommunity.recipes.telescope-lsp-mappings" },
+  { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.recipes.picker-lsp-mappings" },
   { import = "astrocommunity.colorscheme.catppuccin" },
-  -- import/override with your plugins folder
 }
